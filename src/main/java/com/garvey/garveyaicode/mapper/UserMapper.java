6f8d9ca6872cb57469738bce9f0f6aa55ec76ecd
Garvey_lai <1325121571@qq.com>
2026-03-11 14:52:29 +0800

    补充mapper

---
+++ src/main/java/com/garvey/garveyaicode/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.garvey.garveyaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.garvey.garveyaicode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
